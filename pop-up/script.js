56de391c028ee77cd6fd17f3be6ee76373a7ae9c
Aarshita Acharya <159445412+aarshitaacharya@users.noreply.github.com>
2024-03-03 09:25:18 +0530

    initial commit

---
+++ pop-up/script.js
@@ -0,0 +1,10 @@
+const popup = document.getElementById("popup");
+const submitBtn = document.querySelector(".btn");
+
+function openPopup(){
+    popup.classList.add("open-popup");
+}
+
+function closePopup(){
+    popup.classList.remove("open-popup");
+}
